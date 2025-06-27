5aa357cd2124d7888393e48ecbc1a425412d77cc
Etchu Ferdinand <ferdinandetchu@gmail.com>
2025-06-27 14:55:37 +0000

    remove the previes implementation

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,6 @@ import type {Metadata} from 'next';
 import './globals.css';
 import { Toaster } from "@/components/ui/toaster";
 import { ThemeProvider } from '@/components/theme-provider';
-import { AnimatedBackground } from '@/components/animated-background';
 
 export const metadata: Metadata = {
   title: 'Ferdinand Etchu - Software Engineer & Mentor',
@@ -22,7 +21,6 @@ export default function RootLayout({
         <link href="https://fonts.googleapis.com/css2?family=Jost:wght@400;500;700&display=swap" rel="stylesheet" />
       </head>
       <body className="font-body antialiased">
-        <AnimatedBackground />
         <ThemeProvider
           attribute="class"
           defaultTheme="system"
