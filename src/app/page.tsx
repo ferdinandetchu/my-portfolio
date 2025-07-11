253b63fd305b2f53dadee0d962062edc896a6eba
Etchu Ferdinand <ferdinandetchu@gmail.com>
2025-07-11 10:57:52 +0000

    work experince of PaveWay Academy is from 2020 not 2023

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -98,7 +98,7 @@ const workExperience = [
     company: 'PaveWay Academy',
     companyLink: 'https://academy.pavewaygroup.com',
     role: 'Front End Development Tutor',
-    duration: '2023 - Present',
+    duration: '2020 - Present',
     description: 'Educating and mentoring aspiring developers in core front-end technologies and best practices.',
     logo: 'https://placehold.co/100x100.png',
     logoHint: 'online learning',
